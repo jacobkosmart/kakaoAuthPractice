c58d58fda2986d91b9c6edafa5aa8a5a02c75a19
jacobkosmart <jacobkosmart@gmail.com>
2022-08-02 09:37:37 +1200

    Initial Commit

---
+++ KakaoAuth/KakaoAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoAuthApp.swift
+//  KakaoAuth
+//
+//  Created by Jacob Ko on 2022/08/02.
+//
+
+import SwiftUI
+
+@main
+struct KakaoAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
